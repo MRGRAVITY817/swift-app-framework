246aaa70d6aa42684a95b90dd778796162ee32ce
Hoon Wee <mrgravity817@gmail.com>
2023-08-06 23:52:12 +0900

    fix: change ContentView -> FrameworkGridView

--- AppFramework/AppFrameworkApp.swift
+++ AppFramework/AppFrameworkApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AppFrameworkApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FrameworkGridView()
         }
     }
 }
---
+++ AppFramework/FrameworkGridView.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworkGridView.swift
+//  AppFramework
+//
+//  Created by Hoon Wee on 2023/08/06.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+    }
+}
