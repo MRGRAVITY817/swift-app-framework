6e6a9d36efab4468ce383a96e002f90460c65d1e
Hoon Wee <mrgravity817@gmail.com>
2023-08-06 23:41:43 +0900

    Initial Commit

---
+++ AppFramework/AppFrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppFrameworkApp.swift
+//  AppFramework
+//
+//  Created by Hoon Wee on 2023/08/06.
+//
+
+import SwiftUI
+
+@main
+struct AppFrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
