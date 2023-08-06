619a039bfbcc1fa042dddf9326df5527cf910f9d
Hoon Wee <mrgravity817@gmail.com>
2023-08-07 00:05:26 +0900

    feat: add grid list

--- AppFramework/FrameworkGridView.swift
+++ AppFramework/FrameworkGridView.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        LazyVGrid(columns: columns) {
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+        }
     }
 }
 
@@ -17,4 +30,25 @@ struct FrameworkGridView_Previews: PreviewProvider {
     static var previews: some View {
         FrameworkGridView()
     }
+}
+
+struct FrameworkTitleView: View {
+    var name: String
+    var imageName: String
+    
+    var body: some View {
+        VStack {
+            Image(imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            
+            Text(name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.5)
+        }
+
+    }
+    
 }
