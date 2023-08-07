ef004fb19e301d27642d153ec00433927929a1f2
Hoon Wee <mrgravity817@gmail.com>
2023-08-08 02:18:43 +0900

    feat: add safari view when clicking learn more button

--- AppFramework/FrameworkDetailView.swift
+++ AppFramework/FrameworkDetailView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct FrameworkDetailView: View {
     var framework: Framework
     @Binding var isShowingDetailView: Bool
+    @State private var isShowingSafariView = false
     
     var body: some View {
         VStack {
@@ -37,10 +38,15 @@ struct FrameworkDetailView: View {
             Spacer()
             
             Button {
+                isShowingSafariView = true
             } label: {
                 AFButton(title: "Learn More")
             }
         }
+        .fullScreenCover(isPresented: $isShowingSafariView,
+               content: {
+                 SafariView(url: URL(string: framework.urlString) ?? URL(string: "www.apple.com")!)
+               })
     }
 }
 
