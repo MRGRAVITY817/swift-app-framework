4e92f9d92195c123a1cbd4342360b32c82651e38
Hoon Wee <mrgravity817@gmail.com>
2023-08-08 01:54:56 +0900

    feat: add SFViewController, adopting UIKit with SwiftUI

---
+++ AppFramework/SafariView.swift
@@ -0,0 +1,22 @@
+//
+//  SafariView.swift
+//  AppFramework
+//
+//  Created by Hoon Wee on 2023/08/08.
+//
+
+import SwiftUI
+import SafariServices
+
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView> ) {
+    }
+}
